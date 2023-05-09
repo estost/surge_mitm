45543c353d7645cd67a647106f7f537ad27b5f92
estost <106503309+estost@users.noreply.github.com>
2023-05-09 22:15:12 +0800

    Update and rename letterboxd_ken_lite.js to letterboxd_bon_lite.js

---
+++ letterboxd_bon_lite.js
@@ -0,0 +1,82 @@
+{
+  "id": "BUMy",
+  "name": "The King of Minami: The Movie I",
+  "originalName": "難波金融伝 ミナミの帝王 劇場版I 銭の一・二",
+  "sortingName": "king of minami: the movie i0000the",
+  "alternativeNames": [],
+  "releaseYear": 1993,
+  "poster": {
+    "sizes": [
+      {
+        "width": 35,
+        "height": 52,
+        "url": "https://a.ltrbxd.com/resized/film-poster/9/0/3/6/4/1/903641-the-king-of-minami-the-movie-i-0-35-0-52-crop.jpg?v=281202974f"
+      },
+      {
+        "width": 70,
+        "height": 105,
+        "url": "https://a.ltrbxd.com/resized/film-poster/9/0/3/6/4/1/903641-the-king-of-minami-the-movie-i-0-70-0-105-crop.jpg?v=281202974f"
+      },
+      {
+        "width": 150,
+        "height": 225,
+        "url": "https://a.ltrbxd.com/resized/film-poster/9/0/3/6/4/1/903641-the-king-of-minami-the-movie-i-0-150-0-225-crop.jpg?v=281202974f"
+      },
+      {
+        "width": 300,
+        "height": 450,
+        "url": "https://a.ltrbxd.com/resized/film-poster/9/0/3/6/4/1/903641-the-king-of-minami-the-movie-i-0-300-0-450-crop.jpg?v=281202974f"
+      },
+      {
+        "width": 600,
+        "height": 900,
+        "url": "https://a.ltrbxd.com/resized/film-poster/9/0/3/6/4/1/903641-the-king-of-minami-the-movie-i-0-600-0-900-crop.jpg?v=281202974f"
+      }
+    ]
+  },
+  "adult": false,
+  "reviewsHidden": false,
+  "filmCollectionId": "2kKZ",
+  "links": [
+    {
+      "type": "letterboxd",
+      "id": "BUMy",
+      "url": "https://letterboxd.com/film/the-king-of-minami-the-movie-i/"
+    },
+    {
+      "type": "tmdb",
+      "id": "1003055",
+      "url": "https://www.themoviedb.org/movie/1003055"
+    },
+    {
+      "type": "justwatch",
+      "id": "1003055",
+      "url": "https://www.justwatch.com/"
+    }
+  ],
+  "genres": [],
+  "runTime": 84,
+  "originalLanguage": {
+    "code": "ja",
+    "name": "Japanese"
+  },
+  "languages": [
+    {
+      "code": "ja",
+      "name": "Japanese"
+    }
+  ],
+  "contributions": [
+    {
+      "type": "Director",
+      "contributors": [
+        {
+          "id": "1IM3",
+          "name": "Sadaaki Haginiwa",
+          "tmdbid": "1204050"
+        }
+      ]
+    }
+  ],
+  "recentStories": []
+}
